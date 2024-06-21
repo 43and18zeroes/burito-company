cb0795f30b9cfd203c8a809b6bffc1d0f44073ae
Christoph <c.wagner.germany@gmail.com>
2024-06-21 15:31:22 +0200

    21June8

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const productList = {
-    products: {
-        title: 'Big Burito',
-        imageUrl: 'img/01.jpg',
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    }
-}
+  products: [
+    {
+      title: 'Big Burito',
+      imageUrl: 'img/01.jpg',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+  ],
+  render() {
+    const renderHook = document.getElementById('product-container');
+  },
+};
