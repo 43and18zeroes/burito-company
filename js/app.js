0a2dca8b730afc7793ba0eb323693688f6994f10
Christoph <c.wagner.germany@gmail.com>
2024-06-21 15:52:23 +0200

    21June14

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ const productList = {
       description:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
+    {
+      title: 'Saucy Burito',
+      imageUrl: 'img/02.jpg',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
   ],
   render() {
     const renderHook = document.getElementById('product-container');
