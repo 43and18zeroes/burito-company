efe0f52744e978aa3b690292b38c9b9cf4f90b9f
Christoph <c.wagner.germany@gmail.com>
2024-06-21 15:46:43 +0200

    21June12

--- js/app.js
+++ js/app.js
@@ -19,14 +19,11 @@ const productList = {
             <div class="card__content">
                 <h3>${prod.title}</h3>
                 <div id="product-container" class="content__description">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a type
-                    specimen book.
+                    ${prod.description}
                 </div>
             </div>
         `;
     }
   },
 };
+// productList.render();
