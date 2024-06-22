6a0d61d18243632eca810f13ca91fbc0c6f8e1c3
Christoph <c.wagner.germany@gmail.com>
2024-06-22 14:35:39 +0200

    22June9

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 class Product {
-  constructor(title, imageUrl, description) {
+  constructor(title, image, desc) {
     this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
+    this.imageUrl = image;
+    this.description = desc;
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'result__card';
+    prodEl.innerHTML = `
+      <div class="image__container">
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" />
+      </div>
+      <div class="card__content">
+          <h3>${this.product.title}</h3>
+          <div id="product-container" class="content__description">
+              ${this.product.description}
+          </div>
+      </div>
+    `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       'Big Burito',
       'img/01.jpg',
@@ -33,28 +56,21 @@ const productList = {
       'img/05.jpg',
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     ),
-  ],
+  ];
+
+  constructor() {};
+
   render() {
     const renderHook = document.getElementById('product-container');
     const prodList = document.createElement('ul');
     prodList.className = 'content__results';
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'result__card';
-      prodEl.innerHTML = `
-        <div class="image__container">
-          <img src="${prod.imageUrl}" alt="${prod.title}" />
-        </div>
-        <div class="card__content">
-            <h3>${prod.title}</h3>
-            <div id="product-container" class="content__description">
-                ${prod.description}
-            </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
+  }
 };
+const productList = new ProductList();
 productList.render();
