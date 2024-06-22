3585aa2bea9f3a08faf2ccada618551e348d9fe4
Christoph <c.wagner.germany@gmail.com>
2024-06-22 14:24:54 +0200

    22June8

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
 class Product {
-  title;
-  imageUrl;
-  description;
-
   constructor(title, imageUrl, description) {
     this.title = title;
     this.imageUrl = imageUrl;
@@ -15,27 +11,27 @@ const productList = {
     new Product(
       'Big Burito',
       'img/01.jpg',
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     ),
     new Product(
       'Saucy Burito',
       'img/02.jpg',
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     ),
     new Product(
       'Hot Burito',
       'img/03.jpg',
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     ),
     new Product(
       'Burito Bunch',
       'img/04.jpg',
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     ),
     new Product(
       'Burito Deluxe',
       'img/05.jpg',
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     ),
   ],
   render() {
@@ -46,16 +42,16 @@ const productList = {
       const prodEl = document.createElement('li');
       prodEl.className = 'result__card';
       prodEl.innerHTML = `
-            <div class="image__container">
-              <img src="${prod.imageUrl}" alt="${prod.title}" />
+        <div class="image__container">
+          <img src="${prod.imageUrl}" alt="${prod.title}" />
+        </div>
+        <div class="card__content">
+            <h3>${prod.title}</h3>
+            <div id="product-container" class="content__description">
+                ${prod.description}
             </div>
-            <div class="card__content">
-                <h3>${prod.title}</h3>
-                <div id="product-container" class="content__description">
-                    ${prod.description}
-                </div>
-            </div>
-        `;
+        </div>
+      `;
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
