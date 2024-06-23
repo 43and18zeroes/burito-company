fb65bb7eeac161f2c9de1b9c2ec58c0f76327870
Christoph <c.wagner.germany@gmail.com>
2024-06-23 12:03:29 +0200

    22June15

--- js/app.js
+++ js/app.js
@@ -122,15 +122,16 @@ class ProductItem extends Component {
 }
 
 class ProductList extends Component {
-  products = [];
+  #products = [];
 
   constructor(renderHookId) {
-    super(renderHookId);
+    super(renderHookId, false);
+    this.render();
     this.fetchProducts();
   }
 
   fetchProducts() {
-    this.products = [
+    this.#products = [
       new Product(
         'Big Burito',
         'img/01.jpg',
@@ -166,7 +167,7 @@ class ProductList extends Component {
   }
 
   renderProducts() {
-    for (const prod of this.products) {
+    for (const prod of this.#products) {
       new ProductItem(prod, 'prod-list');
     }
   }
@@ -175,7 +176,7 @@ class ProductList extends Component {
     this.createRootElement('ul', 'content__results', [
       new ElementAttribute('id', 'prod-list'),
     ]);
-    if (this.products && this.products.length > 0) {
+    if (this.#products && this.#products.length > 0) {
       this.renderProducts();
     }
   }
