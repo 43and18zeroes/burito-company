ca4fe0eab03fe1b306151787430ad9b664657939
Christoph <c.wagner.germany@gmail.com>
2024-06-24 16:48:52 +0200

    24June7

--- js/app.js
+++ js/app.js
@@ -241,12 +241,19 @@ async function fetchData() {
 }
 fetchData();
 
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-    setTimeout(resolve, 100, 'foo');
-});
 
 Promise.all([promise1, promise2, promise3]).then(values => {
     console.log(values);
+});
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, 'one');
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'two');
+});
+
+Promise.race([promise1, promise2]).then(value => {
+  console.log(value); // 'two'
 });
